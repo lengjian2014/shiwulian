ad796864ceb1f851a01e92fcadc5532025e1cfb0
lengjian2014 <lgj1029@126.com>
2015-12-11 14:53:45 +0800

    1.2.0

---
+++ web/frontend/config/main.php
@@ -0,0 +1,42 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+		'urlManager' => [
+		    'enablePrettyUrl' => true,
+		    'showScriptName' => false,
+		    //'suffix' => '.html',
+		    'rules' => [
+			    '<controller:\w+>/<action:\w+>/<id:\w+>'=>'<controller>/<action>',
+			    '<module>/<controller:\w+>/<action:\w+>/<id:\d+>'=>'<module>/<controller>/<action>',
+		    ],
+	    ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];

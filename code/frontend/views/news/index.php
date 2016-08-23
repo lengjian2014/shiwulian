143e440d34e069ed22580c34bd85b0a6019bf2f3
lengjian2014 <lgj1029@126.com>
2016-08-23 18:31:32 +0800

    2.0.2

--- code/frontend/views/news/index.php
+++ code/frontend/views/news/index.php
@@ -4,15 +4,161 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('app', '行业资讯'), 'url
 $this->params['breadcrumbs'][] = '最新资讯';
 
 ?>
-<div class="row">
-<div class="col-md-9">
-	<div style="background-color: #fff; padding-bottom: 50px;">
-	<ul class="daodu nav">
-		<li class="on">所有动态</li>
-		<li>产品详情</li>
-		<li>累计评价</li>
-		<li>追溯记录</li>
-	</ul>
+<div class="news-list">
+	<div class="row">
+		<div class="col-md-8">
+			<img alt="900x500" width="100%" height="340px" data-src="holder.js/900x500/auto/#777:#777" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8jNzc3OiM3NzcKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNjZjZDhlNCB0ZXh0IHsgZmlsbDojNzc3O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjQ1cHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2NmNkOGU0Ij48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjxnPjx0ZXh0IHg9IjMzMy4yMTY2NjcxNzUyOTI5NyIgeT0iMjcwLjQiPjkwMHg1MDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+		</div>
+		<div class="col-md-4">
+			<div style="margin-bottom: 10px;">
+				<img alt="900x500" width="100%" height="165px" data-src="holder.js/900x500/auto/#777:#777" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8jNzc3OiM3NzcKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNjZjZDhlNCB0ZXh0IHsgZmlsbDojNzc3O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjQ1cHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2NmNkOGU0Ij48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjxnPjx0ZXh0IHg9IjMzMy4yMTY2NjcxNzUyOTI5NyIgeT0iMjcwLjQiPjkwMHg1MDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+			</div>
+			<img alt="900x500" width="100%" height="165px" data-src="holder.js/900x500/auto/#777:#777" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8jNzc3OiM3NzcKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNjZjZDhlNCB0ZXh0IHsgZmlsbDojNzc3O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjQ1cHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2NmNkOGU0Ij48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjxnPjx0ZXh0IHg9IjMzMy4yMTY2NjcxNzUyOTI5NyIgeT0iMjcwLjQiPjkwMHg1MDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+		</div>
 	</div>
-</div>
+	<div class="row" style="margin-top:20px;margin-bottom: 100px;">
+	<div class="col-md-8" style="margin-bottom: 20px;">
+		<div class="inbrowse"></div>
+		<div class="browsetag">
+			<span>资讯分类：</span>
+			<a class="active">全部</a>
+			<a>时政要闻</a>
+			<a>行业预测</a>
+		</div>
+		<div class="news-list-media">
+			<div class="media">
+				<div class="media-left">
+					<a href="#"><img class="media-object" alt="64x64" data-src="holder.js/64x64" style="width: 192px; height: 150px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNjhlOGIxMiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2OGU4YjEyIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"></a>
+				</div>
+				<div class="media-body">
+					<h4 class="media-heading"><a>对自美进口白羽肉鸡产品反补贴措施期终复审裁决</a></h4>
+					<p><a>裁定自美国进口白羽肉鸡产品受到的补贴可能继续或再度发生，如果终止反补贴措施，对中国国内产业造成的损害可能继续或再度发生，并决定维持该反补贴措施，为期5年。</a></p>
+					<p><span class="label label-default">进口白羽肉鸡</span><span class="label label-default">反补贴措施</span></p>
+				</div>
+			</div>
+			<ul class="card-handle">
+				<li><a href="javascript:;"><font class="glyphicon glyphicon-eye-open"></font><span>4482</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-comment"></i><span>2</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-thumbs-up"></i><span>7</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-star"></i><span>收藏</span></a></li>
+			</ul>
+		</div>
+		<div class="news-list-media">
+			<div class="media">
+				<div class="media-left">
+					<a href="#"><img class="media-object" alt="64x64" data-src="holder.js/64x64" style="width: 192px; height: 150px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNjhlOGIxMiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2OGU4YjEyIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"></a>
+				</div>
+				<div class="media-body">
+					<h4 class="media-heading"><a>对自美进口白羽肉鸡产品反补贴措施期终复审裁决</a></h4>
+					<p><a>裁定自美国进口白羽肉鸡产品受到的补贴可能继续或再度发生，如果终止反补贴措施，对中国国内产业造成的损害可能继续或再度发生，并决定维持该反补贴措施，为期5年。</a></p>
+					<p><span class="label label-default">进口白羽肉鸡</span><span class="label label-default">反补贴措施</span></p>
+				</div>
+			</div>
+			<ul class="card-handle">
+				<li><a href="javascript:;"><font class="glyphicon glyphicon-eye-open"></font><span>4482</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-comment"></i><span>2</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-thumbs-up"></i><span>7</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-star"></i><span>收藏</span></a></li>
+			</ul>
+		</div>
+		<div class="news-list-media">
+			<div class="media">
+				<div class="media-left">
+					<a href="#"><img class="media-object" alt="64x64" data-src="holder.js/64x64" style="width: 192px; height: 150px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNjhlOGIxMiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2OGU4YjEyIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"></a>
+				</div>
+				<div class="media-body">
+					<h4 class="media-heading"><a>对自美进口白羽肉鸡产品反补贴措施期终复审裁决</a></h4>
+					<p><a>裁定自美国进口白羽肉鸡产品受到的补贴可能继续或再度发生，如果终止反补贴措施，对中国国内产业造成的损害可能继续或再度发生，并决定维持该反补贴措施，为期5年。</a></p>
+					<p><span class="label label-default">进口白羽肉鸡</span><span class="label label-default">反补贴措施</span></p>
+				</div>
+			</div>
+			<ul class="card-handle">
+				<li><a href="javascript:;"><font class="glyphicon glyphicon-eye-open"></font><span>4482</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-comment"></i><span>2</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-thumbs-up"></i><span>7</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-star"></i><span>收藏</span></a></li>
+			</ul>
+		</div>
+		<div class="news-list-media">
+			<div class="media">
+				<div class="media-left">
+					<a href="#"><img class="media-object" alt="64x64" data-src="holder.js/64x64" style="width: 192px; height: 150px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNjhlOGIxMiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2OGU4YjEyIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"></a>
+				</div>
+				<div class="media-body">
+					<h4 class="media-heading"><a>对自美进口白羽肉鸡产品反补贴措施期终复审裁决</a></h4>
+					<p><a>裁定自美国进口白羽肉鸡产品受到的补贴可能继续或再度发生，如果终止反补贴措施，对中国国内产业造成的损害可能继续或再度发生，并决定维持该反补贴措施，为期5年。</a></p>
+					<p><span class="label label-default">进口白羽肉鸡</span><span class="label label-default">反补贴措施</span></p>
+				</div>
+			</div>
+			<ul class="card-handle">
+				<li><a href="javascript:;"><font class="glyphicon glyphicon-eye-open"></font><span>4482</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-comment"></i><span>2</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-thumbs-up"></i><span>7</span></a></li>
+				<li><a class="" href="javascript:;"><i class="glyphicon glyphicon-star"></i><span>收藏</span></a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class=" nav-left">
+			<ul class=" nav nav-border-left">
+				<li class="on">最新资讯</li>
+			</ul>
+			<div class="media">
+				<div class="media-left"><a href="#"><img class="media-object" alt="64x64" data-src="holder.js/64x64" style="width: 86px; height: 75px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNmE5ZmQ4ZiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2YTlmZDhmIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"></a></div>
+				<div class="media-body">
+					<h4 class="media-heading"><a>对自美进口白羽肉鸡产品反补贴措施期终复审裁决</a></h4>
+				</div>
+			</div>
+			<div class="media">
+				<ul class="list-unstyled">
+					<li><a>农业部迅速贯彻落实李克强总理重要批示精神</a></li>
+					<li><a>大连市蓝莓提质实现产销两旺</a></li>
+					<li><a>大连市蓝莓提质实现产销两旺</a></li>
+					<li><a>大连市蓝莓提质实现产销两旺</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class=" nav-left">
+			<ul class=" nav nav-border-left">
+				<li class="on">行情分析</li>
+			</ul>
+			<div class="media">
+				<div class="media-left"><a href="#"><img class="media-object" alt="64x64" data-src="holder.js/64x64" style="width: 86px; height: 75px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTZiNmE5ZmQ4ZiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NmI2YTlmZDhmIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"></a></div>
+				<div class="media-body">
+					<h4 class="media-heading"><a>对自美进口白羽肉鸡产品反补贴措施期终复审裁决</a></h4>
+				</div>
+			</div>
+			<div class="media">
+				<ul class="list-unstyled">
+					<li><a>农业部迅速贯彻落实李克强总理重要批示精神</a></li>
+					<li><a>大连市蓝莓提质实现产销两旺</a></li>
+					<li><a>大连市蓝莓提质实现产销两旺</a></li>
+					<li><a>大连市蓝莓提质实现产销两旺</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class=" nav-left">
+			<ul class=" nav nav-border-left">
+				<li class="on">热门标签</li>
+			</ul>
+			<div class="media popular-tags">
+				<a href="/tag/4#pvareaid=2125120" target="_blank"> 最新车讯</a>
+<a href="/tag/5#pvareaid=2125120" target="_blank"> 评测体验</a>
+<a href="/tag/6#pvareaid=2125120" target="_blank"> 热点聚焦</a>
+<a href="/tag/7#pvareaid=2125120" target="_blank"> 选车指南</a>
+<a href="/tag/9#pvareaid=2125120" target="_blank"> 汽车旅行</a>
+<a href="/tag/11#pvareaid=2125120" target="_blank"> 文化历史</a>
+<a href="/tag/12#pvareaid=2125120" target="_blank"> 黑科技</a>
+<a href="/tag/13#pvareaid=2125120" target="_blank"> 技术详解</a>
+<a href="/tag/14#pvareaid=2125120" target="_blank"> 越野</a>
+<a href="/tag/15#pvareaid=2125120" target="_blank"> 探险</a>
+<a href="/tag/16#pvareaid=2125120" target="_blank"> 车事杂谈</a>
+<a href="/tag/18#pvareaid=2125120" target="_blank"> 海外车坛</a>
+<a href="/tag/19#pvareaid=2125120" target="_blank"> 行业分析</a>
+<a href="/tag/20#pvareaid=2125120" target="_blank"> 新鲜趣闻</a>
+<a href="/tag/21#pvareaid=2125120" target="_blank"> 真人秀</a>
+			</div>
+		</div>
+	</div>
+	</div>
 </div>
